d1e0f0a3ffda53f5b0f7a15c0ddc238fdcd1cf59
Categio <categio1@gmail.com>
2023-01-25 17:57:58 -0800

    Add project files.

---
+++ Coin Toss/Coin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coin_Toss
+{
+    class Coin
+    {
+        //field to represent the the side facing up; 
+        private string sideUp;
+
+        //RNG
+        Random rand = new Random();
+
+        //Constructor
+        public Coin()
+        {
+            sideUp = "Heads";
+        }
+
+        //the toss method simulates the coin toss
+        public void Toss()
+        {
+            //use random number ot determine the sid eof the coin that is facing up.
+            //0=Heads, 1=Tails
+
+            if (rand.Next(2) == 0)
+            {
+                sideUp = "Heads";
+            }
+            else
+            {
+                sideUp = "Tails";
+            }
+        }
+
+        //The GetSideUp method returns the value of the sideUp field.
+
+        public string GetSideUp()
+        {
+            return sideUp;
+        }
+    }
+}
